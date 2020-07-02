4f89e28dcb0578459d80b2dcc8fe30b42cb96789
Lubomir <lubomir.yudenko@gmail.com>
2020-07-02 17:39:47 +0300

    Remove memfs completly

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,3 @@
-//Freeze shim
-import { fs } from "memfs"
-Object.freeze = function <T>(obj: T[]): readonly T[] {
-	var props = Object.getOwnPropertyNames(obj)
-
-	for (var i = 0; i < props.length; i++) {
-		var desc = Object.getOwnPropertyDescriptor(obj, props[i])
-
-		if ("value" in desc) {
-			desc.writable = false
-		}
-
-		desc.configurable = false
-		Object.defineProperty(obj, props[i], desc)
-	}
-
-	return obj
-}
-
 g.clear()
 g.drawRect(10, 10, 50, 70)
 g.fillRect(50, 50, 200, 200)
@@ -24,7 +5,5 @@ g.setFontVector(20)
 g.drawString("ebic", 10, 10)
 g.flip()
 
-fs.writeFile("cool.txt", "E", () => {})
-
-g.drawString(fs.readFileSync("cool.txt", {}), 0, 0)
+g.drawString("E", 0, 0)
 g.flip()
